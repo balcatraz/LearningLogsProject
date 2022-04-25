17e2bafb105fcdaf13836597dfdfa6baea65e7e8
Brandon Alcaraz <brandon_alcaraz1@baylor.edu>
2022-04-25 15:02:22 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# learning log is project folder
+#
+# setting up django environment
+#   django-admin startproject learning_log .    // setsup django project (folder) for use
+#   py manage.py migrate                        // applies changes to databases
+#   py manage.py runserver                      // runs server
+#   py manage.py makemigrations                 // creates changes to databases
+#   py manage.py startapp MainApp               // creates an app (folder)
+#   py manage.py createsuperuser                // creates an admin
+#
+# add app to settings in project folder
